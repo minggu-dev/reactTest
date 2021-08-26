8db96c182aa62e3cebba3e5511ed879980d27b08
minho <61855672+minggu-dev@users.noreply.github.com>
2021-08-26 14:35:50 +0900

    간단한 JSX문법 살펴보기

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,6 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+// const element = <div>This is JSX</div>;
+// const element = <img src={user.avatarUrl}></img>;
+const element = (
+  <div>
+    <h1>JSX</h1>
+    <div>여러줄은 괄호로 묶어서 사용할 수 있습니다.</div>
+  </div>
+);
+
+function greeting(name){
+  if(name === "react") // 등호표시 주의
+  return "hello! " + name;
+  else
+  return "hello! guest";
+}
+
 function App() {
   return (
     <div className="App">
@@ -8,6 +24,12 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Hi~
+          { element }
+
+          <h1>
+            {greeting("react1")}
+          </h1>
         </p>
         <a
           className="App-link"
