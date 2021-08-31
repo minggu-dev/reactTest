f27c05356ea475024dd75675f609586337545bbb
minho <61855672+minggu-dev@users.noreply.github.com>
2021-08-31 14:00:21 +0900

    event 예제 다뤄보기

--- hello/src/components/test.js
+++ hello/src/components/test.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 
 class Test extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: true };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn,
+    }));
+  }
   render() {
-    return <div>test component</div>;
+    return (
+      <div>
+        test component
+        <br />
+        <botton onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'On' : 'Off'}
+        </botton>
+      </div>
+    );
   }
 }
 export { Test };
